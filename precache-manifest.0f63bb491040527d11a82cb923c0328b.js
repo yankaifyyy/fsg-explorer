4438ff4dfb5f8604e7c75b6161683c75877ca214
Kai Yan <yankaifyyy@163.com>
2020-05-26 15:44:06 +0800

    Demo page

---
+++ precache-manifest.0f63bb491040527d11a82cb923c0328b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df59009ef2d6c785300f4ffa6386401e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "80e5cd1adb3cceb35bcb",
+    "url": "./static/css/2.fa7d55d0.chunk.css"
+  },
+  {
+    "revision": "47ca08d0878aa47012de",
+    "url": "./static/css/main.bd0bf586.chunk.css"
+  },
+  {
+    "revision": "80e5cd1adb3cceb35bcb",
+    "url": "./static/js/2.64bfa774.chunk.js"
+  },
+  {
+    "revision": "47ca08d0878aa47012de",
+    "url": "./static/js/main.25b55979.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
